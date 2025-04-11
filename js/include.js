2e3c5f869bd3c3bdb346062fdebdb3056f78c613
Jitendra <nerdcode@Jitendras-MacBook-Air.local>
2025-04-11 14:51:55 +0530

    restarting the project after deleting all

--- js/include.js
+++
@@ -1,8 +0,0 @@
-function loadHTML(id, file) {
-  fetch(file)
-    .then(response => response.text())
-    .then(data => document.getElementById(id).innerHTML = data);
-}
-
-loadHTML("header", "header.html");
-loadHTML("footer", "footer.html");
